bd51d14d489be7e49fa171107507ec4c5f848605
Dave Judd <52185dj@gmail.com>
2023-05-10 17:12:48 -0500

    Add List component

---
+++ src/components/List.jsx
@@ -0,0 +1,22 @@
+'use client';
+import { useState } from 'react';
+
+export default function List() {
+  const [items,setItems] = useState([
+    "Bananas",
+    "Fresh Thyme Peanut Butter",
+    "Spam Singles"
+  ])
+
+  return (
+    <ul className="flex flex-col space-y-4 w-full max-w-md">
+      {items.map((item, index) => {
+        return (
+          <li key={index} className="text-md border-2 border-slate-500">
+            <button className="w-full text-left p-2">{item}</button>
+          </li>
+        )
+      })}
+    </ul>
+  )
+}
